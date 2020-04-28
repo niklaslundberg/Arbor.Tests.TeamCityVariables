4af2fdd8b49b0f8820966d747f2a3b111eb39376
Niklas Lundberg <info@nlundberg.se>
2020-04-28 17:59:27 +0200

    Initial commit.

---
+++ TeamCityVariables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Serilog;
+
+namespace TeamCityVariables
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            using var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
+            logger.Information("##teamcity[dynamicParameterLogger 'logger1']");
+
+            logger.Information("##teamcity[dynamicParameterConsole 'console1']");
+
+            return 0;
+        }
+    }
+}
